8a574f88bb09ba0634869f7c62208f129c83b53d
SayfullaevSirojiddin <-email@example.com>
2025-07-03 15:05:52 +0500

    commit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,15 @@
 import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
+
+
+import Header from '../components/layoutComponents/header/Header';
+import Navbar from '../components/layoutComponents/navbar/Navbar';
+import Footer from '../components/layoutComponents/footer/Footer';
+import LeftSidebar from '../components/layoutComponents/sidebars/leftSidebar/LeftSidebar';
+import RightSidebar from '../components/layoutComponents/sidebars/rightSidebar/RightSidebar';
  
+
 export default async function LocaleLayout({
   children,
   params
@@ -15,10 +23,25 @@ export default async function LocaleLayout({
   }
  
   return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
-      </body>
-    </html>
+    <div>
+        <NextIntlClientProvider>
+
+          <Header/>
+          <Navbar/>
+            <div  className='bg-[#F5F7FA]'>
+              <div className="container">
+                <div className='grid grid-cols-[1fr_3fr_1fr] gap-8 py-8'>
+                  <LeftSidebar />
+                    {children}
+                  <RightSidebar />
+                </div>
+              </div>
+            </div>
+            
+            
+          <Footer/>
+
+        </NextIntlClientProvider>
+    </div>
   );
 }
---
+++ src/app/components/layoutComponents/sidebars/leftSidebar/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LeftSidebar() {
+  return (
+    <div className='bg-red-600'>
+      LeftSidebar
+    </div>
+  )
+}
